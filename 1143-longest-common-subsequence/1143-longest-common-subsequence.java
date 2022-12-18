6fac0574f3c0af40273627b80d7065e4a987d7e8
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-12-18 22:44:57 +0200

    Time: 12 ms (88.51%), Space: 46.3 MB (34.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,8 +1,5 @@
-class Solution {
-    String s1, s2;
-    int[][] memo;
+class Solution {    
     public int longestCommonSubsequence(String text1, String text2) {
-        /*
         int n = text1.length(), m = text2.length();
         int[][] dp = new int[n + 1][m + 1];
         for (int i = 0; i <= n; i++) {
@@ -16,27 +13,5 @@ class Solution {
             }
         }
         return dp[n][m];
-        */
-        this.s1 = text1;
-        this.s2 = text2;
-        this.memo = new int[s1.length()][s2.length()];
-        return LCS(0, 0);
-    }
-    
-    private int LCS(int i, int j) {
-        if (i == s1.length() || j == s2.length())
-            return 0;
-        
-        if (memo[i][j] != 0)
-            return memo[i][j];
-        
-        if (s1.charAt(i) == s2.charAt(j))
-            return 1 + LCS(i + 1, j + 1);
-        
-        int choice1 = LCS(i + 1, j);
-        int choice2 = LCS(i, j + 1);
-        
-        memo[i][j] = Math.max(choice1, choice2);
-        return memo[i][j];
     }
 }
