53d6da1261391d3bdf285a1f922dd00e0d6db5c6
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-12-25 17:36:44 +0200

    Time: 4 ms (99.87%), Space: 42.8 MB (92.57%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -6,6 +6,7 @@ class Solution {
         for (int i = 1; i < nums.length; i++)
             nums[i] += nums[i - 1];
         
+        // binary search
         int[] result = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int insertion = Arrays.binarySearch(nums, queries[i]);
