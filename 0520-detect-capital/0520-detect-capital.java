b6e65e635d936f3308b647c3edc41c8da380e9e3
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-01-02 09:32:47 +0200

    Time: 2 ms (69.88%), Space: 40.4 MB (95.10%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        return word.matches("[A-Z]*|.[a-z]*");
+        return word.toLowerCase().equals(word) ||
+               word.toUpperCase().equals(word) ||
+               (Character.isUpperCase(word.charAt(0)) &&
+                word.substring(1).toLowerCase().equals(word.substring(1)));
     }
 }
