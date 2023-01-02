cb532ca691676116501496c613955349ea708088
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-01-02 09:44:42 +0200

    Time: 1 ms (100.00%), Space: 41.1 MB (69.49%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,9 +1,25 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        String temp = word.substring(1);
-        return word.toLowerCase().equals(word) ||
-               word.toUpperCase().equals(word) ||
-               (Character.isUpperCase(word.charAt(0)) &&
-                temp.toLowerCase().equals(temp));
+        boolean isFirstCap = Character.isUpperCase(word.charAt(0));
+        if (isFirstCap) {
+            if (word.length() > 1) {
+                boolean isSecondCap = Character.isUpperCase(word.charAt(1));
+                if (isSecondCap) {
+                    for (int i = 2; i < word.length(); i++)
+                        if (Character.isLowerCase(word.charAt(i)))
+                            return false;
+                } else {
+                    for (int i = 2; i < word.length(); i++)
+                        if (Character.isUpperCase(word.charAt(i)))
+                            return false;
+                }
+            } else
+                return true;
+        } else {
+            for (int i = 1; i < word.length(); i++)
+                if (Character.isUpperCase(word.charAt(i)))
+                    return false;
+        }
+        return true;
     }
 }
