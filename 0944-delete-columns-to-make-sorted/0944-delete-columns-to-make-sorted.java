9b1fbcbc924aa34e01d4aac31c808079d5220e61
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-01-03 14:01:44 +0200

    Time: 8 ms (93.03%), Space: 42.2 MB (89.44%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -2,8 +2,8 @@ class Solution {
     public int minDeletionSize(String[] strs) {
         int counter = 0, col = strs[0].length();
         for (int c = 0; c < col; c++) {
-            for (int r = 0; r < strs.length - 1; r++) {
-                if (strs[r].charAt(c) > strs[r + 1].charAt(c)) {
+            for (int r = 1; r < strs.length; r++) {
+                if (strs[r - 1].charAt(c) > strs[r].charAt(c)) {
                     counter++;
                     break;
                 }
