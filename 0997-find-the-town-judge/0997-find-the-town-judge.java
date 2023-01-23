1eb2cf2e99085740cd1c06cc533b4da656baa8af
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-01-23 13:44:51 +0200

    Time: 11 ms (41.15%), Space: 74.2 MB (15.92%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,20 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if (trust.length == 0 && n == 1)
-            return 1;
-        else {
-            HashMap<Integer, Integer> freq = new HashMap<>();
-            HashSet<Integer> persons = new HashSet<>();
-            for (int i = 0; i < trust.length;  i++) {
-                freq.put(trust[i][1], freq.getOrDefault(trust[i][1], 0) + 1);
-                persons.add(trust[i][0]);
-            }
-            if (n - persons.size() > 1)
-                return -1;
-            for (int i = 0; i < trust.length;  i++)
-                if (freq.get(trust[i][1]) == persons.size())
-                    return trust[i][1];
-            return -1;
-        }
+        int[] freq = new int[n + 1];
+        for (int i = 0; i < trust.length; i++)
+            freq[trust[i][1]]++;
+        int judge = -1;
+        boolean isExist = true;
+        for (int i = 0; i <= n; i++)
+            if(freq[i] == n - 1)
+                judge = i;
+        for (int i = 0; i < trust.length; i++)
+            if (trust[i][0] == judge)
+                isExist = false;
+        if (isExist) return judge;
+        return -1;
     }
 }
