b6b93cdc746999541f57c515364180922007353a
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-01-30 13:45:07 +0200

    Time: 0 ms (100.00%), Space: 39 MB (70.39%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -4,12 +4,12 @@ class Solution {
             return 0;
         if (n == 1 || n == 2)
             return 1;
-        int t0 = 0, t1 = 1, t2 = 1, temp;
+        int t0 = 0, t1 = 1, t2 = 1;
         for (int i = 1; i <= n - 2; i++) {
-            temp = t2;
-            t2 = t0 + t1 + t2;
+            int temp = t0 + t1 + t2;
             t0 = t1;
-            t1 = temp;
+            t1 = t2;
+            t2 = temp;
         }
         return t2;
     }
