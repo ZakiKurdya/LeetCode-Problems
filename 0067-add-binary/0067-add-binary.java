eb958c2a0ef9a71928e00ced36d4103a9961ab54
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-02-14 18:49:41 +0200

    Time: 1 ms (100.00%), Space: 40.9 MB (82.84%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,17 +4,16 @@ class Solution {
         int carry = 0;
         StringBuilder sb = new StringBuilder();
         while (i >= 0 || j >= 0) {
-        int sum = carry;
-        if (i >= 0)
-            sum += a.charAt(i--) - '0';
-        if (j>=0)
-            sum += b.charAt(j--) - '0';
-        sb.append(sum % 2);
-        carry = sum / 2;
+            int sum = carry;
+            if (i >= 0)
+                sum += a.charAt(i--) - '0';
+            if (j>=0)
+                sum += b.charAt(j--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
         if(carry != 0)
             sb.append(carry);
-
         return sb.reverse().toString();    
     }
 }
