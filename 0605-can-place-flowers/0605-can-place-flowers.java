833dca0b6b0f18a473a580f0f47aaf7315919213
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-03-20 16:13:19 +0200

    Time: 1 ms (100.00%), Space: 43.4 MB (55.09%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,17 +1,23 @@
-class Solution {
+public class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
-        for (int i = 0; i < flowerbed.length && count < n; i++) {
+        
+        for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0) {
-               int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1]; 
-               int prev = (i == 0) ? 0 : flowerbed[i - 1];
-               if (next == 0 && prev == 0) {
-                   flowerbed[i] = 1;
-                   count++;
-               }
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) ||
+                                         (flowerbed[i + 1] == 0);
+            
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    count++;
+                    
+                    if (count >= n)
+                        return true;
+                }
             }
         }
         
-        return count == n;
+        return count >= n;
     }
 }
