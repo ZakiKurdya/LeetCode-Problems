807cd26fdab6fea7b4d48ae5475a89834664ed8e
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-05-24 21:06:32 +0300

    Time: 3 ms (97.95%), Space: 42.2 MB (97.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -10,21 +10,19 @@ class Solution {
         
         outer:
         while (l < r) {
-            while ((int) s.charAt(l) > 122 ||
-                   ((int) s.charAt(l) < 97 && (int) s.charAt(l) > 57) ||
-                   (int) s.charAt(l) < 48) {
+            int left = (int) s.charAt(l);
+            while (left > 122 || left < 48 || (left < 97 && left > 57)) {
                 l++;
                 continue outer;
             }
             
-            while ((int) s.charAt(r) > 122 ||
-                   ((int) s.charAt(r) < 97 && (int) s.charAt(r) > 57) ||
-                   (int) s.charAt(r) < 48) {
+            int right = (int) s.charAt(r);
+            while (right > 122 || right < 48 || (right < 97 && right > 57)) {
                 r--;
                 continue outer;
             }
             
-            if (s.charAt(l) == s.charAt(r)) {
+            if (left == right) {
                 l++;
                 r--;
             } else
