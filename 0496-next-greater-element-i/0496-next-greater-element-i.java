00f3669f1733d7d92f911a50d2ba34401f459d45
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-05-25 01:05:02 +0300

    Time: 5 ms (33.17%), Space: 43.9 MB (5.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,49 @@
 class Solution {
+// 99% solution    
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i = 0; i < nums2.length; i++)
+//             map.put(nums2[i], i);
+        
+//         for (int i = 0; i < nums1.length; i++) {
+//             int index = map.get(nums1[i]), value = -1;
+            
+//             for (int j = index + 1; j < nums2.length; j++) {
+//                 if (nums2[j] > nums2[index]) {
+//                     value = nums2[j];
+//                     break;
+//                 }
+//             }
+            
+//             nums1[i] = value;
+//         }
+        
+//         return nums1;
+//     }
+    
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums2.length; i++)
-            map.put(nums2[i], i);
+        for (int i = 0; i < nums1.length; i++)
+            map.put(nums1[i], i);
         
-        for (int i = 0; i < nums1.length; i++) {
-            int index = map.get(nums1[i]), value = -1;
-            
-            for (int j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums2[index]) {
-                    value = nums2[j];
-                    break;
-                }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums2[0]);
+        
+        int[] result = new int[nums1.length];
+        for (int i = 1; i < nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                int temp = stack.pop();
+                if (map.containsKey(temp) == true)
+                    result[map.get(temp)] = nums2[i];
             }
             
-            nums1[i] = value;
+            if (map.containsKey(nums2[i]))
+                stack.push(nums2[i]);
         }
+                
+        while (!stack.isEmpty())
+            result[map.get(stack.pop())] = -1;
         
-        return nums1;
+        return result;
     }
 }
