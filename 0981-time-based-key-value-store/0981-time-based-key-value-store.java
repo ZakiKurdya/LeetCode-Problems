71cb6b1c9655802a97380684161de9341f099526
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-05-28 14:42:53 +0300

    Time: 156 ms (77.28%), Space: 118.2 MB (38.66%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,28 +1,58 @@
 class TimeMap {
-    private Map<String, TreeMap<Integer, String>> timeMap;
+    private Map<String, List<Entry>> timeMap;
 
     public TimeMap() {
         timeMap = new HashMap<>();
     }
 
     public void set(String key, String value, int timestamp) {
-        TreeMap<Integer, String> temp = timeMap.getOrDefault(key, new TreeMap<>());
-        temp.put(timestamp, value);
-        
-        timeMap.put(key, temp);
+        List<Entry> entryList = timeMap.getOrDefault(key, new ArrayList<>());
+        entryList.add(new Entry(timestamp, value));
+        timeMap.put(key, entryList);
     }
 
     public String get(String key, int timestamp) {
         if (!timeMap.containsKey(key))
             return "";
 
-        TreeMap<Integer, String> keyMap = timeMap.get(key);
-        // greatest key less than or equal to given key
-        Integer floorTimestamp = keyMap.floorKey(timestamp);
+        List<Entry> entryList = timeMap.get(key);
+        int index = binarySearch(entryList, timestamp);
 
-        if (floorTimestamp == null)
+        if (index == -1)
             return "";
 
-        return keyMap.get(floorTimestamp);
+        return entryList.get(index).value;
+    }
+
+    // Binary search implementation to find the index of the greatest entry less than or equal to the given timestamp
+    private int binarySearch(List<Entry> entryList, int timestamp) {
+        int left = 0;
+        int right = entryList.size() - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            Entry entry = entryList.get(mid);
+
+            if (entry.timestamp <= timestamp) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    // Helper class to represent an entry with a timestamp and value
+    private static class Entry {
+        int timestamp;
+        String value;
+
+        Entry(int timestamp, String value) {
+            this.timestamp = timestamp;
+            this.value = value;
+        }
     }
 }
