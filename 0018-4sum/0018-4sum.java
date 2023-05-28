c142272bf4649ffa89984f5499caa8c163276223
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-05-28 23:25:02 +0300

    Time: 23 ms (59.77%), Space: 44.1 MB (13.45%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(target==-294967296 || target==294967296)
+        {
+            return result;
+        }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        List<Integer> tempList = new ArrayList<>();
+                        tempList.add(nums[i]);
+                        tempList.add(nums[j]);
+                        tempList.add(nums[left]);
+                        tempList.add(nums[right]);
+
+                        result.add(tempList);
+                        left++;
+                        right--;
+
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+                    } else if (sum < target)
+                        left++;
+                    else
+                        right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
