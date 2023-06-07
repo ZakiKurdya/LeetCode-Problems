f21100c0fc5d09784f798aa4c9655ae2ef0de8fd
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2023-06-07 11:27:45 +0300

    Time: 174 ms (24.18%), Space: 120.2 MB (12.66%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,56 +1,34 @@
 class TimeMap {
-    private static class MapNode {
-        int timestamp;
-        String value;
-
-        public MapNode(int timestamp, String value) {
-            this.timestamp = timestamp;
-            this.value = value;
-        }
-    }
-    
-    private Map<String, List<MapNode>> timeMap;
+    private Map<String, TreeMap<Integer, String>> map;
 
     public TimeMap() {
-        timeMap = new HashMap<>();
+        this.map = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        List<MapNode> temp = timeMap.getOrDefault(key, new ArrayList<>());
-        temp.add(new MapNode(timestamp, value));
+        TreeMap temp = map.getOrDefault(key, new TreeMap<Integer, String>());
+        temp.put(timestamp, value);
         
-        timeMap.put(key, temp);
+        map.put(key, temp);
     }
-
+    
     public String get(String key, int timestamp) {
-        if (!timeMap.containsKey(key))
+        if (!map.containsKey(key))
             return "";
-
-        List<MapNode> temp = timeMap.get(key);
         
-        int index = lowerBound(temp, timestamp);
-
-        if (index == -1)
+        TreeMap<Integer, String> temp = map.get(key);
+        Integer idx = (Integer) temp.floorKey(timestamp);
+        
+        if (idx == null)
             return "";
-
-        return temp.get(index).value;
-    }
-
-    // greatest timestamp less than or equal to the given timestamp
-    private int lowerBound(List<MapNode> list, int timestamp) {
-        int left = 0, right = list.size() - 1, result = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            MapNode item = list.get(mid);
-
-            if (item.timestamp <= timestamp) {
-                result = mid;
-                left = mid + 1;
-            } else
-                right = mid - 1;
-        }
-
-        return result;
+        
+        return temp.get(idx);
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
